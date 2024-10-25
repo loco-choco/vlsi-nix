ea89767bbab93f171fa3ffeb75369a8c7b0ee556
Locochoco <ivanrwpf@gmail.com>
2024-10-25 00:42:08 -0300

    added patcher

--- openpdk/buildOpenPDK.nix
+++ openpdk/buildOpenPDK.nix
@@ -1,5 +1,5 @@
 tecnology:
-{ stdenv, magic-vlsi, python3, fetchFromGitHub, fetchgit }:
+{ stdenv, magic-vlsi, python3, tk, tcsh, fetchFromGitHub, fetchgit }:
 let
   version = "1.0.498";
   pdks-flags = import ./open-pdks.nix;
@@ -7,7 +7,10 @@ let
     "--enable-${source}-${tecnology}=${
       fetchgit pdks-flags.${tecnology}.sources.${source}
     }") (builtins.attrNames (pdks-flags.${tecnology}.sources)));
-commit-substitute-flags = 
+  flag-substitute-file = builtins.toFile "flag-substitute.sed"
+    ''s/$(shell cd ''${.*} ; git rev-parse HEAD)/"unknown"/g'';
+  pythonRuntime = python3.withPackages (ps: with ps; [ setuptools ]);
+  #commit-substitute-flags = 
 in stdenv.mkDerivation {
   name = "open-pdk-${tecnology}";
   src = fetchFromGitHub {
@@ -16,18 +19,24 @@ in stdenv.mkDerivation {
     rev = "${version}";
     hash = "sha256-g1f3I0I3ECbCvDCNa6HQhla3ykEHRyafqS4xL2P01V8=";
   };
-  buildInputs = [ python3 magic-vlsi ];
+  buildInputs = [ pythonRuntime magic-vlsi tk tcsh stdenv];
+  #  sed '/%%%%%NIX_SKY130_MAKEFILE_EDIT%%%%%/{
+  #    s/%%%%%NIX_SKY130_MAKEFILE_EDIT%%%%%//g
+  #    r ${commit-substitute-flags}
+  #  }' ./sky130/Makefile.in
 
   configurePhase = ''
-    echo "Editing SKY130 Makefile.in"
-    sed '/%%%%%NIX_SKY130_MAKEFILE_EDIT%%%%%/{
-      s/%%%%%NIX_SKY130_MAKEFILE_EDIT%%%%%//g
-      r ${commit-substitute-flags}
-    }' ./sky130/Makefile.in
+    echo "Patching SKY130 Makefile.in"
+    sed -i -f ${flag-substitute-file} ./sky130/Makefile.in
+    echo "Patching Shebangs"
+    patchShebangs .
     echo "Configuring PDK Files"
     ./configure --enable-${tecnology}-pdk ${sources-to-flags}
   '';
   #./configure 
+  postBuildPhase = ''
+    ls .
+  '';
 
 }
 
