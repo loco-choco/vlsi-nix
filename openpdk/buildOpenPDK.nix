ee19c4ffc39b0ac7901d721fb2ab216044f5e791
loco-choco <contact@locochoco.dev>
2025-07-20 20:12:04 -0300

    updated formatting

--- openpdk/buildOpenPDK.nix
+++ openpdk/buildOpenPDK.nix
@@ -1,17 +1,26 @@
 tecnology:
-{ stdenv, magic-vlsi, python3, tk, tcsh, fetchFromGitHub, fetchgit }:
+{
+  stdenv,
+  magic-vlsi,
+  python3,
+  tk,
+  tcsh,
+  fetchFromGitHub,
+  fetchgit,
+}:
 let
   version = "1.0.498";
   pdks-flags = import ./open-pdks.nix;
-  sources-to-flags = builtins.concatStringsSep " " (builtins.map (source:
-    "--enable-${source}-${tecnology}=${
-      fetchgit pdks-flags.${tecnology}.sources.${source}
-    }") (builtins.attrNames (pdks-flags.${tecnology}.sources)));
-  flag-substitute-file = builtins.toFile "flag-substitute.sed"
-    ''s/$(shell cd ''${.*} ; git rev-parse HEAD)/"unknown"/g'';
+  sources-to-flags = builtins.concatStringsSep " " (
+    builtins.map (
+      source: "--enable-${source}-${tecnology}=${fetchgit pdks-flags.${tecnology}.sources.${source}}"
+    ) (builtins.attrNames (pdks-flags.${tecnology}.sources))
+  );
+  flag-substitute-file = builtins.toFile "flag-substitute.sed" ''s/$(shell cd ''${.*} ; git rev-parse HEAD)/"unknown"/g'';
   pythonRuntime = python3.withPackages (ps: with ps; [ setuptools ]);
-  #commit-substitute-flags = 
-in stdenv.mkDerivation {
+  #commit-substitute-flags =
+in
+stdenv.mkDerivation {
   name = "open-pdk-${tecnology}";
   src = fetchFromGitHub {
     owner = "RTimothyEdwards";
@@ -19,7 +28,13 @@ in stdenv.mkDerivation {
     rev = "${version}";
     hash = "sha256-g1f3I0I3ECbCvDCNa6HQhla3ykEHRyafqS4xL2P01V8=";
   };
-  buildInputs = [ pythonRuntime magic-vlsi tk tcsh stdenv];
+  buildInputs = [
+    pythonRuntime
+    magic-vlsi
+    tk
+    tcsh
+    stdenv
+  ];
   #  sed '/%%%%%NIX_SKY130_MAKEFILE_EDIT%%%%%/{
   #    s/%%%%%NIX_SKY130_MAKEFILE_EDIT%%%%%//g
   #    r ${commit-substitute-flags}
@@ -33,7 +48,7 @@ in stdenv.mkDerivation {
     echo "Configuring PDK Files"
     ./configure --enable-${tecnology}-pdk ${sources-to-flags}
   '';
-  #./configure 
+  #./configure
   postBuildPhase = ''
     ls .
   '';
