7e481ded27e662edb4f68e7053986b70a686a5dd
loco-choco <contact@locochoco.dev>
2025-07-24 18:22:45 -0300

    made build work

--- openpdk/buildSky130.nix
+++ openpdk/buildSky130.nix
@@ -1,5 +1,5 @@
 {
-  stdenv,
+  stdenvNoCC,
   magic-vlsi,
   python312,
   tk,
@@ -15,43 +15,75 @@ let
     inherit fetchFromGitHub;
     inherit fetchgit;
   };
-  sources-to-flags = builtins.concatStringsSep " " (
-    builtins.map (source: "--enable-${source}-${tecnology}=${pdk-sources.${source}}") (
-      builtins.attrNames pdk-sources
-    )
-  );
-  pythonRuntime = python312;
-in
-stdenv.mkDerivation {
-  name = "open-pdk-${tecnology}";
-
-  src = fetchFromGitHub {
+  open-pdk-script = fetchFromGitHub {
     owner = "RTimothyEdwards";
     repo = "open_pdks";
     rev = "${version}";
     leaveDotGit = true;
     hash = "sha256-n0L0+Ksor76ZkLkynRdgSyCTrYe0maeOEOaikvAKu3c=";
+    name = "open-pdk-script";
   };
+
+  sources = builtins.map (source: pdk-sources.${source}) (builtins.attrNames pdk-sources);
+  source-flags = (
+    builtins.map (source: "--enable-${source}-${tecnology}=../../${source}") (
+      builtins.attrNames pdk-sources
+    )
+  );
+  pythonRuntime = python312;
+in
+stdenvNoCC.mkDerivation {
+  name = "open-pdk-${tecnology}";
+
+  srcs = [
+    open-pdk-script
+  ] ++ sources;
+  sourceRoot = "open-pdk-script";
+  #sourceRoot = open-pdk-script;
   buildInputs = [
     pythonRuntime
     magic-vlsi
     tk
     tcsh
-    stdenv
+    stdenvNoCC
     git
   ];
+
+  #unpackPhase = ''
+  #  runHook preUnpack
+
+  #  for _src in $srcs; do
+  #    cp -r --no-preserve=mode "$_src" $(stripHash "$_src")
+  #  done
+
+  #  runHook postUnpack
+  #'';
 
   #makeFlags = [
   #  "SHARED_PDKS_PATH=$(out)/share/pdk"
   #  "ENABLED_VARIANTS=A"
   #];
-
-  configurePhase = ''
+  preConfigure = ''
+    echo "Making all the folders writable"
+    ${
+      builtins.concatStringsSep ";" (
+        builtins.map (source: "chmod -R u+w ../${source}") (builtins.attrNames pdk-sources)
+      )
+    }  
     echo "Patching Shebangs"
     patchShebangs .
-    echo "Configuring PDK Files"
-    ./configure --enable-${tecnology}-pdk ${sources-to-flags} --prefix=$(out)
   '';
+  #configurePhase = ''
+  #  echo "Patching Shebangs"
+  #  patchShebangs .
+  #  echo "Configuring PDK Files"
+  #  ./configure --enable-${tecnology}-pdk ${sources-to-flags} --prefix=$(out)
+  #'';
+
+  configureFlags = [
+    #"--with-sky130-link-targets=source"
+    "--enable-${tecnology}-pdk"
+  ] ++ source-flags;
 
   #preBuild = ''
   #'';
--- openpdk/sky130.nix
+++ openpdk/sky130.nix
@@ -5,7 +5,8 @@
     repo = "skywater-pdk-libs-sky130_fd_pr";
     rev = "refs/heads/main";
     hash = "sha256-UJCiXaxJQMSXFsnqq0HLzkc5YOidozpsuzW8XoY2lVs=";
-    leaveDotGit = true;
+    leaveDotGit = true;    
+    name = "primitive";
   };
   io = fetchFromGitHub {
     owner = "efabless";
@@ -13,6 +14,7 @@
     rev = "refs/heads/master";
     hash = "sha256-9g5jkGT0hdfqGePZMV131rCAU/gVr8Czrhr/NUdiAV4=";
     leaveDotGit = true;
+    name = "io";
   };
   sc-hd = fetchFromGitHub {
     owner = "efabless";
@@ -20,6 +22,7 @@
     rev = "refs/heads/master";
     hash = "sha256-WWJFgCaHHmx/hsZ7n40PxBrMswswOtiBXQwkV9UZkT0=";
     leaveDotGit = true;
+    name = "sc-hd";
   };
   sc-hdll = fetchFromGitHub {
     owner = "efabless";
@@ -27,6 +30,7 @@
     rev = "refs/heads/master";
     hash = "sha256-qW4iDNF5PiIldSrkLZZk9nZwq80d1i95D91kJylgWEY=";
     leaveDotGit = true;
+    name = "sc-hdll";
   };
   sc-ms = fetchFromGitHub {
     owner = "efabless";
@@ -34,6 +38,7 @@
     rev = "refs/heads/master";
     hash = "sha256-V8D/Z8s/oOzWK6bUpqAtXlP+KJwtiMc5U03Z444Ntac=";
     leaveDotGit = true;
+    name = "sc-ms";
   };
   sc-ls = fetchFromGitHub {
     owner = "efabless";
@@ -41,6 +46,7 @@
     rev = "refs/heads/master";
     hash = "sha256-0fIZpBOH4MxDQcsdjpawsFGwp9gEEYsSD1F/917m10U=";
     leaveDotGit = true;
+    name = "sc-ls";
   };
   sc-hvl = fetchFromGitHub {
     owner = "efabless";
@@ -48,6 +54,7 @@
     rev = "refs/heads/master";
     hash = "sha256-6YmJYMIJ/0ULHE7DPjh9EUE+7BfuaJ3O9oLrLVFa3nU=";
     leaveDotGit = true;
+    name = "sc-hvl";
   };
   sc-lp = fetchFromGitHub {
     owner = "efabless";
@@ -55,6 +62,7 @@
     rev = "refs/heads/master";
     hash = "sha256-Ebfwf8FjZy2wiJBltk35ZReCOkefjCb65NPDm0HyqVU=";
     leaveDotGit = true;
+    name = "sc-lp";
   };
   sc-hs = fetchFromGitHub {
     owner = "efabless";
@@ -62,6 +70,7 @@
     rev = "refs/heads/master";
     hash = "sha256-z2tmg1CecGfNoNUL3YCGjEYkBMYLW8dRy5XFWXq97XM=";
     leaveDotGit = true;
+    name = "sc-hs";
   };
 
   alpha = fetchFromGitHub {
@@ -70,6 +79,7 @@
     rev = "refs/heads/master";
     hash = "sha256-F7rNmKO50hdbDuFvd/8kspZVT0V1ryqqBiLek13ioVY=";
     leaveDotGit = true;
+    name = "alpha";
   };
 
   xschem = fetchFromGitHub {
@@ -78,6 +88,7 @@
     rev = "refs/heads/main";
     hash = "sha256-077qQRGNyc7DsPqKwPGUxPMWlva+ipCbhZ1wirasnTk=";
     leaveDotGit = true;
+    name = "xschem";
   };
 
   klayout = fetchFromGitHub {
@@ -86,6 +97,7 @@
     rev = "refs/heads/main";
     hash = "sha256-x+NgPVZ+01WH7PQr9+r/0R1xzjAzZpEzcL6UO5pFnxg=";
     leaveDotGit = true;
+    name = "klayout";
   };
 
   precheck = fetchFromGitHub {
@@ -94,6 +106,7 @@
     rev = "refs/heads/main";
     hash = "sha256-HslEzw9TPJlKEg66aSDv6780hdnaTq5JB0678RxZKIM=";
     leaveDotGit = true;
+    name = "precheck";
   };
 
   sram = fetchFromGitHub {
@@ -102,6 +115,7 @@
     rev = "refs/heads/main";
     hash = "sha256-mBImCXvd3nZvvl0pD5pH7VHvM8/C934Q9TW4GDsFLQQ=";
     leaveDotGit = true;
+    name = "sram";
   };
 
   sram-space = fetchgit {
@@ -110,6 +124,7 @@
     rev = "refs/heads/main";
     hash = "sha256-sJ9tEHUGZz8bcBJragSjCg2tQFjCPjLjlUAhfVIG42E=";
     leaveDotGit = true;
+    name = "sram-space";
   };
 
   reram = fetchgit {
@@ -118,6 +133,7 @@
     rev = "refs/heads/main";
     hash = "sha256-+HMb+mDsbyDvxQu33HSfLCpRUsYtqU14VCpQsoyFxQk=";
     leaveDotGit = true;
+    name = "reram";
   };
 
   osu-t12 = fetchgit {
@@ -126,6 +142,7 @@
     rev = "refs/heads/main";
     hash = "sha256-RUizbSYI4zIkGhEgrpurWWIBeqaBBEloFgz61usFbow=";
     leaveDotGit = true;
+    name = "osu-t12";
   };
 
   osu-t15 = fetchgit {
@@ -134,6 +151,7 @@
     rev = "refs/heads/main";
     hash = "sha256-oa4k8uq1Jr2YvR+5WfjZcnp45lukrbFx1iv4Qs4amMY=";
     leaveDotGit = true;
+    name = "osu-t15";
   };
 
   osu-t18 = fetchgit {
@@ -142,5 +160,6 @@
     rev = "refs/heads/main";
     hash = "sha256-fRAUtMUMWo1LWGPqQa5EjqjRzX7QnsZBrhCXj1xcbCw=";
     leaveDotGit = true;
+    name = "osu-t18";
   };
 }
